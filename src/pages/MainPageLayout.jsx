5bd81d8352d99c5131dffcbf44ebc4684d06f897
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-12-04 17:49:51 +0300

    introLine

--- src/pages/MainPageLayout.jsx
+++ src/pages/MainPageLayout.jsx
@@ -5,9 +5,9 @@ const MainPageLayout = ({ header, footer, children }) => {
   return (
     <div className="mainPageLayout">
       <div className="header">{header}</div>
-      <div className="body">
+      <div className="main">
         <div className="headerPlug"></div>
-        {children}
+        {children} 
       </div>
       <div className="footer">{footer}</div>
     </div>
--- src/widgets/MainPageContent/Intro.jsx
+++ src/widgets/MainPageContent/Intro.jsx
@@ -4,17 +4,14 @@ import ContentColumn from "../../components/ContentColumn";
 const Intro = () => {
   return (
     <div className="intro">
-      <div className="introContent">
-        <ContentColumn>
-          <h1>
-            Maper объединяет все ваши задачи, партнеров по команде и инструменты
-          </h1>
-          <p>
-            Храните все в одном месте - даже если ваша команда там не находится.
-          </p>
-        </ContentColumn>
-      </div>
-      <div className="introWave">{""}</div>
+      <ContentColumn>
+        <h1>
+          Maper объединяет все ваши задачи, партнеров по команде и инструменты
+        </h1>
+        <p>
+          Храните все в одном месте - даже если ваша команда там не находится.
+        </p>
+      </ContentColumn>
     </div>
   );
 };
