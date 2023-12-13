5286c549f2fe2e045b141d3bc2120f7372e233df
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-12-13 15:35:43 +0300

    ProductivityPowerhouseGrid

--- src/widgets/MainPageContent/ProductivityPowerhouse.jsx
+++ src/widgets/MainPageContent/ProductivityPowerhouse.jsx
@@ -7,16 +7,35 @@ const ProductivityPowerhouse = () => {
   return (
     <ContentColumn>
       <div className="ProductivityPowerhouse">
-        <h1 className="ProductivityPowerhouseTitle">
-          Мощный инструмент повышения производительности
-        </h1>
+        <h1>Мощный инструмент повышения производительности</h1>
         <p className="ProductivityPowerhouseParagr">
           Простой, гибкий и мощный. Все, что требуется, - это доски объявлений,
           списки и карточки, чтобы получить четкое представление о том, кто что
           делает и что необходимо сделать. Узнайте больше в нашем руководстве по
           началу работы.
         </p>
-        <div className="ProductivityPowerhouseBlock">
+        <div className="ProductivityPowerhouseGrid">
+          <ProductivityPowerhouseCard
+            title={"Доски"}
+            desciption={
+              "Доски Maper позволяют систематизировать задачи и продвигать работу вперед. Одним взглядом вы увидите все, от что нужно сделать до о, да, мы сделали это!"
+            }
+          />
+          <ProductivityPowerhouseCard
+            title={"Доски"}
+            desciption={
+              "Доски Maper позволяют систематизировать задачи и продвигать работу вперед. Одним взглядом вы увидите все, от что нужно сделать до о, да, мы сделали это!"
+            }
+          />
+          <ProductivityPowerhouseCard
+            title={"Доски"}
+            desciption={
+              "Доски Maper позволяют систематизировать задачи и продвигать работу вперед. Одним взглядом вы увидите все, от что нужно сделать до о, да, мы сделали это!"
+            }
+          />
+          <div className="ProductivityPowerhouseImg"></div>
+        </div>
+        {/* <div className="ProductivityPowerhouseBlock">
           <div className="ProductivityPowerhouseCards">
             <ProductivityPowerhouseCard
               title={"Доски"}
@@ -40,7 +59,7 @@ const ProductivityPowerhouse = () => {
           <div className="ProductivityPowerhouseImg">
             <img src={WinterImg} alt="dad" />
           </div>
-        </div>
+        </div> */}
       </div>
     </ContentColumn>
   );
